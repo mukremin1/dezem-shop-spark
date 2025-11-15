321899cb31124a2f224bdfc8103872f33c6f2f25
mukremin1 <115407114+mukremin1@users.noreply.github.com>
2025-11-15 16:56:58 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src") // @ işareti src/ klasörüne işaret eder
+      "@": path.resolve(__dirname, "src")
     }
   }
 });
